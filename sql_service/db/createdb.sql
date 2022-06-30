cbe8927566825de1d167f9ddfcc4447a4e427484
elad2208 <eladhjob@gmail.com>
2022-06-30 23:31:22 +0300

    Update docker-compose files

---
+++ sql_service/db/createdb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `firstName` VARCHAR(45) NOT NULL,
+  `lastName` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `userName` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`userName`));
